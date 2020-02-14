442df9ca2006f4c521b6efbd9770caf71cb889ad
Javier Moreira <moreiraje@vcu.edu>
2020-02-14 11:22:43 -0500

    renamed coins so it works on all machines

--- app/src/main/java/com/example/treasure/StartUpScreen.java
+++ app/src/main/java/com/example/treasure/StartUpScreen.java
@@ -7,12 +7,16 @@ import android.view.View;
 
 public class StartUpScreen extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up_screen);
-    }
 
+
+
+    }
 
 
 
