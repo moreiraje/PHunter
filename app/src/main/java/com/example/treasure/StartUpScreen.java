50bce69e0990c92bf56053a5483cc53b4244ad0f
mendozam <mendozam@vcu.edu>
2020-03-14 17:51:03 -0400

    ADDED THE CREDITS SCREEN AND REMOVED THE MY HUNTS BUTTON

--- app/src/main/java/com/example/treasure/StartUpScreen.java
+++ app/src/main/java/com/example/treasure/StartUpScreen.java
@@ -10,18 +10,19 @@ import android.view.View;
 public class StartUpScreen extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up_screen);
 
 
-
     }
 
 
-
+    public void toCredits(View view){
+        Intent credits = new Intent(this, MyHunts.class);
+        startActivity(credits);
+    }
     public void continueHunt(View view){
         Intent startHunt = new Intent(this, MapActivity.class);
         startActivity(startHunt);
